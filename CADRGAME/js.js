7d1d7e5d6eb33aa517a39e1473b4a24236a7c010
elCreacionista <58265463+elCreacionista@users.noreply.github.com>
2020-05-23 17:10:09 +0000

    + historial de compras

--- CADRGAME/js.js
+++ CADRGAME/js.js
@@ -23,39 +23,99 @@ const subiracademia = document.querySelector("#subiracademia");
 const subirmuro = document.querySelector("#subirmuro");
 const subirgranja = document.querySelector("#subirgranja");
 
-const pmme = [0,0,0,0]
 const recursospmme = document.querySelector("#pmme");
 
-function StringLevel(nivel){
-if (nivel <= 0) return "nivel  0";
-return "nivel  " + nivel;
-}
-function actualizarRecursos(){
-
-for (let i = 0; i < produccion.length; i++){
-produccion[i] = habilidades.Nrecursos;
+function addhistory(edificio, coste){
+let string = "<b>" + edificio + "</b> coste: " + coste;
+historial.innerHTML += "<br>" + string;
 }
 
-
-pmme[0] += produccion[0];
-recursospmme.childNodes[0].innerHTML = " piedra: " + pmme[0];
-pmme[1] += produccion[1];
-recursospmme.childNodes[1].innerHTML = " madera: " + pmme[1];
-pmme[2] += produccion[2];
-recursospmme.childNodes[2].innerHTML = " monedas: " + pmme[2];
-pmme[3] += produccion[3];
-recursospmme.childNodes[3].innerHTML = " energia: " + pmme[3];
-}
+const historial = document.querySelector("#historial div");
 
 
 let habilidades = {Nrecursos : 1, Ncoliseo : 0, Ntienda : 0,
  Ncuartel : 0, Nmuro : 0, Ngranja : 0, Nayuntamiento : 1, Nacademia : 0,
 Nmision1 : 1, Nmision2 : 0, Nmision3 : 0};
-let produccion = [1,1,1,1]
+
+const puntosedificios = {recursos : 20, coliseo : 50, tienda : 30,
+ cuartel : 50, muro : 25, granja : 30, ayuntamiento : 60, academia : 75};
+
+const pmme = [10000,0,0,0]
+let produccion = [1,1,1,1];
+let reduccion =(50 - habilidades.Nayuntamiento) / 50;
+console.log(reduccion);
+
 
 const intervalEdificios = setInterval(actualizarNiveles, 1000);
-const intervalRecursos = setInterval(actualizarRecursos, 5000);
+const Actualizar = setInterval(actualizar, 100);
+const intervalRecursos = setInterval(producirRecursos, 5000);
 
+
+function StringLevel(nivel){
+if (nivel <= 0) return "nivel  0";
+return "nivel  " + nivel;
+}
+function StringCoste(coste){
+if (coste <= 0) return "coste:  0";
+return "coste:  " + coste;
+}
+
+function actualizar(){
+recursospmme.childNodes[0].innerHTML = " piedra: " + Math.floor(pmme[0]);
+recursospmme.childNodes[1].innerHTML = " madera: " + Math.floor(pmme[1]);
+recursospmme.childNodes[2].innerHTML = " monedas: " + Math.floor(pmme[2]);
+recursospmme.childNodes[3].innerHTML = " energia: " + Math.floor(pmme[3]);
+}
+
+function producirRecursos(){
+for (let i = 0; i < produccion.length; i++){
+produccion[i] = habilidades.Nrecursos;
+pmme[i] += produccion[i];
+}
+}
+
+function actualizarNiveles(){
+recursos.childNodes[5].innerHTML = StringLevel(habilidades.Nrecursos);
+ayuntamiento.childNodes[5].innerHTML = StringLevel(habilidades.Nayuntamiento);
+cuartel.childNodes[5].innerHTML = StringLevel(habilidades.Ncuartel);
+coliseo.childNodes[5].innerHTML = StringLevel(habilidades.Ncoliseo);
+tienda.childNodes[5].innerHTML = StringLevel(habilidades.Ntienda);
+academia.childNodes[5].innerHTML = StringLevel(habilidades.Nacademia);
+muro.childNodes[5].innerHTML = StringLevel(habilidades.Nmuro);
+granja.childNodes[5].innerHTML = StringLevel(habilidades.Ngranja);
+mision1.childNodes[5].innerHTML = StringLevel(habilidades.Nmision1);
+mision2.childNodes[5].innerHTML = StringLevel(habilidades.Nmision2);
+mision3.childNodes[5].innerHTML = StringLevel(habilidades.Nmision3);
+
+subirayuntamiento.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Nayuntamiento);
+subirrecursos.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Nrecursos);
+subircuartel.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Ncuartel);
+subircoliseo.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Ncoliseo);
+subirtienda.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Ntienda);
+subiracademia.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Nacademia);
+subirmuro.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Nmuro);
+subirgranja.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Ngranja);
+
+
+reduccion = (50 - habilidades.Nayuntamiento) / 50;
+
+// COSTES //
+subirayuntamiento.childNodes[3].childNodes[3].innerHTML = StringCoste(puntosedificios.ayuntamiento * (habilidades.Nayuntamiento + 1) * reduccion);
+
+subirrecursos.childNodes[3].childNodes[3].innerHTML = StringCoste(puntosedificios.recursos * (habilidades.Nrecursos + 1) * reduccion);
+
+subircuartel.childNodes[3].childNodes[3].innerHTML = StringCoste(puntosedificios.cuartel * (habilidades.Ncuartel + 1) * reduccion);
+
+subircoliseo.childNodes[3].childNodes[3].innerHTML = StringCoste(puntosedificios.coliseo * (habilidades.Ncoliseo + 1) * reduccion);
+
+subirtienda.childNodes[3].childNodes[3].innerHTML = StringCoste(puntosedificios.tienda * (habilidades.Ntienda + 1) * reduccion);
+
+subiracademia.childNodes[3].childNodes[3].innerHTML = StringCoste(puntosedificios.academia * (habilidades.Nacademia + 1) * reduccion);
+
+subirmuro.childNodes[3].childNodes[3].innerHTML = StringCoste(puntosedificios.muro * (habilidades.Nmuro + 1) * reduccion);
+
+subirgranja.childNodes[3].childNodes[3].innerHTML = StringCoste(puntosedificios.granja * (habilidades.Ngranja + 1) * reduccion);
+}
 
 //  Inicio  //
 
@@ -91,28 +151,9 @@ subirgranja.style.display = variable;
 }
 
 
-function actualizarNiveles(){
-recursos.childNodes[5].innerHTML = StringLevel(habilidades.Nrecursos);
-ayuntamiento.childNodes[5].innerHTML = StringLevel(habilidades.Nayuntamiento);
-cuartel.childNodes[5].innerHTML = StringLevel(habilidades.Ncuartel);
-coliseo.childNodes[5].innerHTML = StringLevel(habilidades.Ncoliseo);
-tienda.childNodes[5].innerHTML = StringLevel(habilidades.Ntienda);
-academia.childNodes[5].innerHTML = StringLevel(habilidades.Nacademia);
-muro.childNodes[5].innerHTML = StringLevel(habilidades.Nmuro);
-granja.childNodes[5].innerHTML = StringLevel(habilidades.Ngranja);
-mision1.childNodes[5].innerHTML = StringLevel(habilidades.Nmision1);
-mision2.childNodes[5].innerHTML = StringLevel(habilidades.Nmision2);
-mision3.childNodes[5].innerHTML = StringLevel(habilidades.Nmision3);
 
-subirayuntamiento.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Nayuntamiento);
-subirrecursos.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Nrecursos);
-subircuartel.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Ncuartel);
-subircoliseo.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Ncoliseo);
-subirtienda.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Ntienda);
-subiracademia.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Nacademia);
-subirmuro.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Nmuro);
-subirgranja.childNodes[1].childNodes[5].innerHTML = StringLevel(habilidades.Ngranja);
-}
+
+
 
 function showmisions(variable){
 if (habilidades.Nmision1 > 0)
@@ -145,6 +186,7 @@ if (habilidades.Ngranja > 0)
 function enterciudad(){
 actual.innerHTML = "CIUDAD";
 ciudad.style.display = "none";
+cartas.style.display = "none";
 mapa.style.display = "block";
 recursos.style.display = "block";
 showedificios("block")
@@ -153,6 +195,7 @@ mostrarsubirniveles("none");
 }
 
 function enterAyuntamiento(){
+actual.innerHTML = "AYUNTAMIENTO";
 mostrarsubirniveles("block");
 mapa.style.display = "none";
 cartas.style.display = "none";
@@ -168,29 +211,73 @@ ciudad.style.display = "none";
 
 }
 
+
+
 function subirMuro(){
-habilidades.Nmuro++;
+let costeactual = puntosedificios.muro * (habilidades.Nmuro + 1) * reduccion;
+if (pmme[0] >= costeactual){
+	habilidades.Nmuro++;
+	pmme[0] -= costeactual;
+addhistory("muro",  costeactual + " piedras");
 }
+}
+
+
 function subirAyuntamiento(){
-habilidades.NAyuntamiento++;
+let costeactual = puntosedificios.ayuntamiento * (habilidades.Nayuntamiento + 1) * reduccion;
+if (pmme[0] >= costeactual){
+	habilidades.Nayuntamiento++;
+	pmme[0] -= costeactual;
+addhistory("ayuntamiento", costeactual + " piedras");
+}
 }
 function subirCuartel(){
-habilidades.Ncuartel++;
+let costeactual = puntosedificios.cuartel * (habilidades.Ncuartel + 1) * reduccion;
+if (pmme[0] >= costeactual){
+	habilidades.Ncuartel++;
+	pmme[0] -= costeactual;
+addhistory("cuartel", costeactual + " piedras");
+}
 }
 function subirTienda(){
-habilidades.Ntienda++;
+let costeactual = puntosedificios.tienda * (habilidades.Ntienda + 1) * reduccion;
+if (pmme[0] >= costeactual){
+	habilidades.Ntienda++;
+	pmme[0] -= costeactual;
+addhistory("tienda", costeactual + " piedras");
+}
 }
 function subirColiseo(){
-habilidades.Ncoliseo++;
+let costeactual = puntosedificios.coliseo * (habilidades.Ncoliseo + 1) * reduccion;
+if (pmme[0] >= costeactual){
+	habilidades.Ncoliseo++;
+	pmme[0] -= costeactual;
+addhistory("coliseo", costeactual + " piedras");
+}
 }
 function subirAcademia(){
-habilidades.Nacademia++;
+let costeactual = puntosedificios.academia * (habilidades.Nacademia + 1) * reduccion;
+if (pmme[0] >= costeactual){
+	habilidades.Nacademia++;
+	pmme[0] -= costeactual;
+addhistory("academia", costeactual + " piedras");
+}
 }
 function subirGranja(){
-habilidades.Ngranja++;
+let costeactual = puntosedificios.granja * (habilidades.Ngranja + 1) * reduccion;
+if (pmme[0] >= costeactual){
+	habilidades.Ngranja++;
+	pmme[0] -= costeactual;
+addhistory("granja", costeactual + " piedras");
+}
 }
 function subirRecursos(){
-habilidades.Nrecursos++;
+let costeactual = puntosedificios.recursos * (habilidades.Nrecursos + 1) * reduccion;
+if (pmme[0] >= costeactual){
+	habilidades.Nrecursos++;
+	pmme[0] -= costeactual;
+addhistory("recursos", costeactual + " piedras");
+}
 }
 
 
@@ -198,6 +285,7 @@ function mostrarmapa(){
 actual.innerHTML = "MAPA";
 ciudad.style.display = "block";
 mapa.style.display = "none";
+cartas.style.display = "block";
 showmisions("block");
 
 showedificios("none")
